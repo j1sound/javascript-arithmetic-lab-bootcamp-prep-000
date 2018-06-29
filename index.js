0b4337cb9f9de028d3a9180d1909686baaa82f75
JOSEPH WELCH <joewelchdrums@gmail.com>
2018-06-29 04:08:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ function subtract(a,b){
 function multiply(a,b){
   return (a*b)
 }
+
+function divide(a,b){
+  return (a/b)
+}
+
+function inc(n){
+  result (n+1)
+}
+
+function dec(n){
+  result (n-1)
+}
+
+function makeInt(n){
+  result parseInt(n)
+  n=10
+}
+
+function preserveDecimal(n){
+  result parseFloat(n)
+}
+
